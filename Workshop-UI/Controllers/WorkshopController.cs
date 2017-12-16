c998fcf902c1f2294565ea7cf40ee97f62236941
Corn Pivotal <cmendoza@pivotal.io>
2017-12-15 19:44:26 -0600

    updates

--- Workshop-UI/Controllers/WorkshopController.cs
+++ Workshop-UI/Controllers/WorkshopController.cs
@@ -4,11 +4,14 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using Steeltoe.Common.Discovery;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Workshop_UI.ViewModels.Workshop;
 
@@ -21,15 +24,17 @@ namespace Workshop_UI.Controllers
         public CloudFoundryApplicationOptions CloudFoundryApplication { get; set; }
         IOptionsSnapshot<FortuneServiceOptions> _fortunesConfig;
         IDiscoveryClient discoveryClient;
+        IDistributedCache RedisCacheStore { get; set; }
 
         // Lab09 Start
         private FortuneServiceCommand _fortunes;
         public WorkshopController(
             ILogger<WorkshopController> logger,
             IOptionsSnapshot<FortuneServiceOptions> config,
-            FortuneServiceCommand fortunes, 
+            FortuneServiceCommand fortunes,
             IOptions<CloudFoundryApplicationOptions> appOptions,
             IOptions<CloudFoundryServicesOptions> servOptions,
+            IDistributedCache cache,
             [FromServices] IDiscoveryClient client
             )
         {
@@ -39,6 +44,7 @@ namespace Workshop_UI.Controllers
             CloudFoundryApplication = appOptions.Value;
             _fortunesConfig = config;
             discoveryClient = client;
+            RedisCacheStore = cache;
         }
         // Lab09 End
         
@@ -48,12 +54,14 @@ namespace Workshop_UI.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Steeltoe()
         {
             _logger?.LogDebug("Steeltoe");
             return View();
         }
 
+        [HttpGet]
         // Lab10 Start
         //[Authorize(Policy = "read.fortunes")] 
         // Lab10 End
@@ -67,20 +75,61 @@ namespace Workshop_UI.Controllers
             var fortune = await _fortunes.RandomFortuneAsync();
             // Lab05 End
 
-            HttpContext.Session.SetString("MyFortune", fortune.Text);
-            var _idxCount = HttpContext.Session.GetInt32($"Index{CloudFoundryApplication.InstanceIndex}");
-            if (_idxCount != null)
-                _idxCount += 1;
+            SortedList<int, int> appInstCount = new SortedList<int, int>();
+            SortedList<int, int> srvInstCount = new SortedList<int, int>();
+            List<string> fortunes = new List<string>();
 
-            HttpContext.Session.SetInt32($"Index{CloudFoundryApplication.InstanceIndex}", _idxCount.GetValueOrDefault(1));
-            ViewData["InstanceCount"] = $"{_idxCount.GetValueOrDefault(1)}";
+            var _fortuneHistory = RedisCacheStore?.GetString("FortuneHistory");
+            if (!string.IsNullOrEmpty(_fortuneHistory))
+                fortunes = JsonConvert.DeserializeObject<List<string>>(_fortuneHistory);
+
+            fortunes.Insert(0, fortune.Text);
+
+            if (fortunes.Count > 10)
+            {
+                fortunes.RemoveAt(10);
+            }
+
+            string fortuneoutput = JsonConvert.SerializeObject(fortunes);
+            RedisCacheStore?.SetString("FortuneHistory", fortuneoutput);
+            
+            HttpContext.Session.SetString("MyFortune", fortune.Text);
+            
+            var _appInstCount = RedisCacheStore?.GetString("AppInstance");
+            if (!string.IsNullOrEmpty(_appInstCount))
+            {
+                _logger?.LogInformation($"App Session Data: {_appInstCount}");
+                appInstCount = JsonConvert.DeserializeObject<SortedList<int, int>>(_appInstCount);
+            }
+
+            var _srvInstCount = RedisCacheStore?.GetString("SrvInstance");
+            if (!string.IsNullOrEmpty(_srvInstCount))
+            {
+                _logger?.LogInformation($"Servlet Session Data: {_srvInstCount}");
+                srvInstCount = JsonConvert.DeserializeObject<SortedList<int, int>>(_srvInstCount);
+            }
+
+            var _count = appInstCount.GetValueOrDefault(CloudFoundryApplication.Instance_Index, 0);
+            appInstCount[CloudFoundryApplication.Instance_Index] = ++_count;
+
+            var _count2 = srvInstCount.GetValueOrDefault(fortune.InstanceIndex, 0);
+            srvInstCount[fortune.InstanceIndex] = ++_count2;
+            
+            string output = JsonConvert.SerializeObject(appInstCount);
+            RedisCacheStore?.SetString("AppInstance", output);
+            string output2 = JsonConvert.SerializeObject(srvInstCount);
+            RedisCacheStore?.SetString("SrvInstance", output2);
+
             ViewData["MyFortune"] = fortune.Text;
             ViewData["FortuneIndex"] = $"{fortune.InstanceIndex}";
             ViewData["FortuneDiscoveryUrl"] = discoveryClient.GetInstances("fortuneService")?[fortune.InstanceIndex]?.Uri?.ToString();
             return View(new CloudFoundryViewModel(
                 CloudFoundryApplication == null ? new CloudFoundryApplicationOptions() : CloudFoundryApplication,
-                CloudFoundryServices == null ? new CloudFoundryServicesOptions() : CloudFoundryServices));
-
+                CloudFoundryServices == null ? new CloudFoundryServicesOptions() : CloudFoundryServices,
+                discoveryClient,
+                appInstCount,
+                srvInstCount,
+                fortunes));
         }
 
         [HttpPost]
--- Workshop-UI/ViewModels/CloudFoundryViewModel.cs
+++ Workshop-UI/ViewModels/CloudFoundryViewModel.cs
@@ -1,16 +1,51 @@
-﻿using Steeltoe.Extensions.Configuration.CloudFoundry;
-
+﻿using Steeltoe.Common.Discovery;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using System.Collections.Generic;
 
 namespace Workshop_UI.ViewModels.Workshop
 {
     public class CloudFoundryViewModel
     {
-        public CloudFoundryViewModel(CloudFoundryApplicationOptions appOptions, CloudFoundryServicesOptions servOptions)
+        public CloudFoundryViewModel(CloudFoundryApplicationOptions appOptions, CloudFoundryServicesOptions servOptions, IDiscoveryClient client, SortedList<int, int> appCounts, SortedList<int, int> srvCounts, List<string> fortunes)
         {
             CloudFoundryServices = servOptions;
             CloudFoundryApplication = appOptions;
+            discoveryClient = client;
+            ServiceInstanceCounts = srvCounts;
+            FortuneHistory = fortunes;
+            AppInstanceCounts = appCounts;
         }
-        public CloudFoundryServicesOptions CloudFoundryServices { get;}
-        public CloudFoundryApplicationOptions CloudFoundryApplication { get;}
+
+        public IDiscoveryClient discoveryClient { get; }
+        public CloudFoundryServicesOptions CloudFoundryServices { get; }
+        public CloudFoundryApplicationOptions CloudFoundryApplication { get; }
+        public SortedList<int, int> ServiceInstanceCounts { get; }
+        public SortedList<int, int> AppInstanceCounts { get; }
+        public List<string> FortuneHistory { get; }
+
+        public List<KeyValuePair<int, int>> GetServiceInstanceCounts()
+        {
+            List<KeyValuePair<int, int>> ret = new List<KeyValuePair<int, int>>();
+            foreach(var key in ServiceInstanceCounts.Keys)
+            {
+                KeyValuePair<int, int> kp = new KeyValuePair<int, int>(key, ServiceInstanceCounts[key]);
+                ret.Add(kp);
+            }
+
+            return ret;
+        }
+
+        public List<KeyValuePair<int, int>> GetAppInstanceCounts()
+        {
+            List<KeyValuePair<int, int>> ret = new List<KeyValuePair<int, int>>();
+            foreach (var key in AppInstanceCounts.Keys)
+            {
+                KeyValuePair<int, int> kp = new KeyValuePair<int, int>(key, AppInstanceCounts[key]);
+                ret.Add(kp);
+            }
+
+            return ret;
+        }
+
     }
 }
